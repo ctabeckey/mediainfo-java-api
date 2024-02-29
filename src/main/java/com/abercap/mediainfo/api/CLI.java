a7c0e67aa6353a8f18367ba9fb0a8e0c9af56b0e
Chris Beckey <cbeckey@gmail.com>
2024-02-29 09:08:15 -0500

    add dump of all properties

--- src/main/java/com/abercap/mediainfo/api/CLI.java
+++ src/main/java/com/abercap/mediainfo/api/CLI.java
@@ -3,21 +3,23 @@ package com.abercap.mediainfo.api;
 import org.apache.commons.cli.*;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CLI {
     private final static Options options;
-    private final static MediaInfo mediaInfo;
+    private static MediaInfo mediaInfo;
 
     static {
         // create Options object
         options = new Options();
-        options.addOption("t", false, "display current time");
-
-        mediaInfo = new MediaInfo();
     }
 
     public static void main(final String[] argv) {
         CommandLineParser parser = new DefaultParser();
+        mediaInfo = new MediaInfo();
+
         try {
             CommandLine cmd = parser.parse(options, argv);
 
@@ -44,16 +46,41 @@ public class CLI {
         }
     }
 
-    private static String getMetadata(File mediaFile) {
+    private static String getMetadata(final File mediaFile) {
         StringBuilder result = new StringBuilder();
+        final String fileIdentifier = mediaFile.getAbsolutePath();
 
         if (mediaInfo.open(mediaFile)) {
-            String codec = mediaInfo.get(MediaInfo.StreamKind.Video, 0, "CodecID");
+            String videoCodecID = mediaInfo.get(MediaInfo.StreamKind.Video, 0, "CodecID");
+            String generalCodecID =  mediaInfo.get(MediaInfo.StreamKind.General, 0, "CodecID");
+            Map<MediaInfo.StreamKind, List<Map<String, String>>> mediaInfoProperties = mediaInfo.snapshot();
 
-            if (codec != null) {
-                result.append("File [" + mediaFile.getAbsolutePath() + "] uses codec: " + codec);
-            } else {
-                result.append("Unable to determine codec used in MP4 file.");
+            result.append(generalCodecID != null
+                    ? "File [" + fileIdentifier + "] uses generalCodecID: " + generalCodecID
+                    : "Unable to determine generalCodecID used in [" + fileIdentifier + "]");
+            result.append(System.lineSeparator());
+            result.append(videoCodecID != null
+                    ? "File [" + fileIdentifier + "] uses videoCodecId: " + videoCodecID
+                    : "Unable to determine videoCodecID used in [" + fileIdentifier + "]");
+            for(MediaInfo.StreamKind streamKind : MediaInfo.StreamKind.values()) {
+                result.append(System.lineSeparator());
+                result.append(streamKind.toString());
+                result.append(System.lineSeparator());
+
+                AtomicInteger streamIndex = new AtomicInteger(0);
+                List<Map<String, String>> streamKindStreams = mediaInfoProperties.get(streamKind);
+                if (streamKindStreams != null) {
+                    streamKindStreams.stream().forEach(streamMap -> {
+                        int currentStreamIndex = streamIndex.getAndAdd(1);
+                        result.append(System.lineSeparator());
+                        result.append(streamKind + "[" + currentStreamIndex + "]");
+
+                        streamMap.entrySet().stream().forEach(mapEntry -> {
+                            result.append(System.lineSeparator());
+                            result.append(mapEntry.getKey() + "=" + mapEntry.getValue());
+                        });
+                    });
+                }
             }
         } else {
             result.append("Failed to open media file '" + mediaFile.getAbsolutePath() + "'.");
